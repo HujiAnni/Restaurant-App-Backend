40319787ae858673ee4b39d8f67ce24ab4acfbfd
Jianni Hu <jh2585@cornell.edu>
2023-07-09 15:21:47 -0400

    update

--- config/server.js
+++ config/server.js
@@ -1,11 +1,18 @@
+// module.exports = ({ env }) => ({
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
+//   url: "https://b928-2600-4041-5e11-2100-bc01-6a7a-34bd-7993.ngrok-free.app/",
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+//   },
+// });
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: "https://b928-2600-4041-5e11-2100-bc01-6a7a-34bd-7993.ngrok-free.app/",
+  url: env("https://strapi-food-ordering-app.herokuapp.com/"),
+  proxy: true,
   app: {
-    keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
