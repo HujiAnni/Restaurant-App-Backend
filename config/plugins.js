f839a1e752d472f3f7f9006b112f6592c0c26910
Jianni Hu <jh2585@cornell.edu>
2023-08-04 12:43:18 -0400

    adjusted cloudinary add-on

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "hemxvbbpi",
+        api_key: "871289613492436",
+        api_secret: "7so1S9S3e4CtJK9Ba0oSe0T-j8s",
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
